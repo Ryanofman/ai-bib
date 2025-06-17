be15373faae6f327a8d3fe4024d4c94a04251abf
Ryanofman <94554752+Ryanofman@users.noreply.github.com>
2025-06-16 22:57:05 -0600

    Delete components/ui directory

--- components/ui/input.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from "react";
-import { cn } from "./utils";
-
-export interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {}
-
-export const Input = React.forwardRef<HTMLInputElement, InputProps>(({ className, ...props }, ref) => (
-  <input ref={ref} className={cn("flex w-full rounded-lg border px-3 py-2 text-sm shadow-sm focus:outline-none focus:ring-2 focus:ring-black", className)} {...props} />
-));
-Input.displayName = "Input";
